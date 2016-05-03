03735c291d74689c09402f1f564e3c7b42d25782
Xane123 <xanem1@gmail.com>
2016-05-03 16:59:58 -0400

    Changed .rc file slightly.

--- src/gitinfo.h
+++ src/gitinfo.h
@@ -1,8 +1,8 @@
-// d1382f1adb25cc87440e19a5a79b0c5d683981cc
+// dd214f4d8516f547d020e22e6f92b505ff5c9c0c
 //
 // GIT_DESCRIPTION (the version number) is located in version.h!
 // Don't edit this by hand as it updates with each compile!
 
-#define GIT_DESCRIPTION "v1.1.1-alpha-38-gd1382f1"
-#define GIT_HASH "d1382f1adb25cc87440e19a5a79b0c5d683981cc"
-#define GIT_TIME "2016-05-02 22:41:40 -0400"
+#define GIT_DESCRIPTION "v1.1.1-alpha-40-gdd214f4"
+#define GIT_HASH "dd214f4d8516f547d020e22e6f92b505ff5c9c0c"
+#define GIT_TIME "2016-05-03 14:28:21 -0400"
