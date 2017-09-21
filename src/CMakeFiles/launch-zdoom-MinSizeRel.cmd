68ad762e17d4bf788ba30409025ea52facb4bf1b
Xane Myers <xanem1@gmail.com>
2017-09-21 18:08:04 -0400

    Removed pointless files.
    Yes, for real, since Im' now building the game outside of the repository. I know @alexey-lysiuk told me I had to start the whole repository over but I feel too lazy so for now I'm just making it cleaner by noting what files moved to the new "build" directory.

--- src/CMakeFiles/launch-zdoom-MinSizeRel.cmd
+++
@@ -1,9 +0,0 @@
-cd C:/Users/Myers/Documents/GitHub/MaryMagicalAdventure
-set PATH=;%PATH%
-
-if [%1]==[--debugger] (
-	SHIFT
-)
-
-"C:\Users\Myers\Documents\GitHub\MaryMagicalAdventure\MinSizeRel\play.exe"  
-pause
